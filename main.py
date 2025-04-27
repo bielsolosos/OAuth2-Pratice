af8947aa72f3e663a89f4f395a34730c312bdf47
Gabriel Coutinho <bielrochasantoscoutinho@gmail.com>
2025-04-26 21:41:04 -0300

    feat: Ajustes para o Deploy

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from typing import Union
-
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
-
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root():
+    return {"message": "API funcionando certinho!"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# Esse trecho abaixo é pra quando você rodar localmente (tipo python main.py)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000)),
+        reload=True
+    )
