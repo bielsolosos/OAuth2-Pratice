5f5204db5d86b520232a934fad9dfd48e495c315
Gabriel Coutinho <bielrochasantoscoutinho@gmail.com>
2025-04-29 19:57:29 -0300

    feat: removendo controller que não é usado

--- app/controllers/auth_test_controller.py
+++ app/controllers/auth_test_controller.py
@@ -4,11 +4,6 @@ from app.models.user import User
 
 router = APIRouter()
 
-@router.get("/users/me")
-def get_me(current_user: User = Depends(get_current_user)):
-    return current_user
-
-
 @router.get("/token-test")
 def test_me(current_user: User = Depends(get_current_user)):
     return {"message": f"Oi, {current_user.name}! Seu token está válido!"}
