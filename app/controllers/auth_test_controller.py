284a4cea31d18bf685aca237d2130c36f31b82dd
Gabriel Coutinho <bielrochasantoscoutinho@gmail.com>
2025-04-29 20:00:56 -0300

    refactor: removendo controller não utilizado

--- app/controllers/auth_test_controller.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Depends
-from app.core.auth_config import get_current_user
-from app.models.user import User
-
-router = APIRouter()
-
-@router.get("/token-test")
-def test_me(current_user: User = Depends(get_current_user)):
-    return {"message": f"Oi, {current_user.name}! Seu token está válido!"}
